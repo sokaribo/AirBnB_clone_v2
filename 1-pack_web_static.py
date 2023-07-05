37522a7a97f4fa8b2ba741ca35cd0f28f60a63e2
sokaribo <cotterellsoks@gmail.com>
2023-07-05 22:20:30 +0000

    0x03.AirBnB-Deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-#  Fabric script that generates a .tgz archive from the
-# contents of the web_static folder of your AirBnB Clone repo
-# using the function do_pack
+# Fabfile to generates a .tgz archive from the contents of web_static.
 import os.path
 from datetime import datetime
 from fabric.api import local
